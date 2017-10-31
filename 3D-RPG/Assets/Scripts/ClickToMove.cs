ceec569547084978fc72563a52b7ad7ab16f80c2
Chong Tan <a46981@gmail.com>
2017-10-31 17:14:53 -0500

    create a player

---
+++ 3D-RPG/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour {
+	public float speed;
+	public CharacterController controller;
+	private Vector3 position;
+	//public AnimationClip run;
+	//public AnimationClip idle;
+	//public static bool attack;
+	//public static bool die;
+	
+	// Use this for initialization
+	void Start () {
+		position = transform.position;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(!attack&&!die)
+		//{
+			//if(Input.GetMouseButton(1))
+			//{
+				//locatePosition();			
+			//}
+			//moveToPosition();
+		//}
+		//else
+		//{
+			
+		//}
+		if (Input.GetMouseButton (0)) 
+		{
+			locatePosition ();
+		}
+		moveToPosition ();
+	}
+	void locatePosition()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit, 1000))
+		{
+			//if(hit.collider.tag!="Player"&&hit.collider.tag!="Enemy")
+			//{
+				position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			//}
+		}
+	}
+	void moveToPosition()
+	{
+		//Game Object is moving;
+		if(Vector3.Distance(transform.position, position)>1)
+		{
+			Quaternion newRotation = Quaternion.LookRotation(position-transform.position, Vector3.forward);
+			newRotation.x= 0f;
+			newRotation.z= 0f;
+			transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime*10);
+			controller.SimpleMove(transform.forward * speed);
+			//transform.GetComponent<Animation>().Play(run.name);
+		}
+		else{
+			//transform.GetComponent<Animation>().Play(idle.name);
+		}
+	}
+}
