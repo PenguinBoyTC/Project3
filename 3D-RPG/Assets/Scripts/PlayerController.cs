f5bd7984695fd611d414f3f42e7eb499ee1e0811
Chong Tan <a46981@gmail.com>
2017-11-28 18:33:07 -0600

    add level up system

--- 3D-RPG/Assets/Scripts/PlayerController.cs
+++ 3D-RPG/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
 	public float experience{ get; private set;}
 	private int level;
-	private Texture levelText;
+	private Text levelText;
 	private Transform experienceBar;
     void Start () {
 		level = 1;
-		//experienceBar = UIController.instance.transform.Find ("Background/Experience");
-		//levelText = UIController.instance.transform.Find ("Background/Level_Text").GetComponent<Text> ();
+		experienceBar = UIController.instance.transform.Find ("Background/Experience");
+		levelText = UIController.instance.transform.Find ("Background/Level_Text").GetComponent<Text> ();
 		SetExperience (0);
 	}
 
@@ -21,17 +22,17 @@ public class PlayerController : MonoBehaviour {
 	}
 	public void SetExperience(float exp){
 		experience += exp;
-		//float experienceNeeded = GameLogic.ExperienceForNextLevel (level);
-		//float previousExperience = GameLogic.ExperienceForNextLevel (level - 1);
-		//if (experience >= experienceNeeded) {
-			//LevelUp ();
-			//experienceNeeded= GameLogic.ExperienceForNextLevel (level);
-			//previousExperience = GameLogic.ExperienceForNextLevel (level - 1);
-		//}
-		//experienceBar.Find ("Fill").GetComponent<Image> ().fillAmount = (experience - previousExperience) / (experienceNeeded - previousExperience);
+		float experienceNeeded = GameLogic.ExperienceForNextLevel (level);
+		float previousExperience = GameLogic.ExperienceForNextLevel (level - 1);
+		if (experience >= experienceNeeded) {
+			LevelUp ();
+			experienceNeeded= GameLogic.ExperienceForNextLevel (level);
+			previousExperience = GameLogic.ExperienceForNextLevel (level - 1);
+		}
+		experienceBar.Find ("Fill_bar").GetComponent<Image> ().fillAmount = (experience - previousExperience) / (experienceNeeded - previousExperience);
 	}
 	void LevelUp(){
 		level++;
-		//levelText.text = "LV." + level.ToString ("00");
+		levelText.text = "LV." + level.ToString ("00");
 	}
 }
