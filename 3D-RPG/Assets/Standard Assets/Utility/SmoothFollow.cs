949b09112d4adf7df3302f383b3ae0310b7037bb
Johnathon Bliss <jbliss9821@gmail.com>
2017-11-28 19:44:07 -0600

    Fixed mall scene and camera

--- 3D-RPG/Assets/Standard Assets/Utility/SmoothFollow.cs
+++ 3D-RPG/Assets/Standard Assets/Utility/SmoothFollow.cs
@@ -9,11 +9,11 @@ namespace UnityStandardAssets.Utility
 		[SerializeField]
 		private Transform target;
 		// The distance in the x-z plane to the target
-		[SerializeField]
-		private float distance = 15.0f;
+
+		public float distance = 25f;
 		// the height we want the camera to be above the target
-		[SerializeField]
-		private float height = 5.0f;
+
+		public float height = 30f;
 
 		[SerializeField]
 		private float rotationDamping;
